97102c2cf8c8e700c9816c9ede97ab74d251a44b
GraN <117268180+nagraiv@users.noreply.github.com>
2024-05-10 00:44:24 +0700

    eslint reconfigured

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,13 +1,15 @@
-import healthRange from '../App/app'
+/* eslint-env jest */
+
+import healthRange from '../App/app';
 
 test.each([
-    ['high', { name: 'Bob', health: 90 }, 'healthy'],
-    ['low', { name: 'John', health: 5 }, 'critical'],
-    ['average', { name: 'Lora', health: 38 }, 'wounded']
+    [ 'high', { name: 'Bob', health: 90 }, 'healthy' ],
+    [ 'low', { name: 'John', health: 5 }, 'critical' ],
+    [ 'average', { name: 'Lora', health: 38 }, 'wounded' ],
 ])(
     ('Тестируем функцию оценки %s здоровья персонажа %s'),
     (range, player, healthStatus) => {
         const result = healthRange(player);
         expect(result).toBe(healthStatus);
-    }
+    },
 );
